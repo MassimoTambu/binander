a06670112b366c9a2e54c91891d98d7a64a7bf45
Massimo Tamburini <massimo.tamburini@botika.ai>
2021-12-29 16:34:57 +0100

    edited snackbar

--- lib/services/snackbar_service.dart
+++ lib/services/snackbar_service.dart
@@ -11,7 +11,12 @@ class SnackBarService {
 
   void show(BuildContext context, String message, {bool dismissable = false}) {
     final snackBar = SnackBar(
-      content: Text(message),
+      duration: const Duration(seconds: 3),
+      content: Text(
+        message,
+        style: TextStyle(color: Theme.of(context).textTheme.bodyText1?.color),
+      ),
+      backgroundColor: Theme.of(context).primaryColor,
       action: SnackBarAction(
         label: 'Okay',
         onPressed: () {},
