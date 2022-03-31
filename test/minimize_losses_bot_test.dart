a3207632d1d4547321479ba2b5234e06450b6ffd
Massimo Tamburini <massimo.tamburini@botika.ai>
2022-03-31 17:19:12 +0200

    feat(test): added riverpod setup

--- test/minimize_losses_bot_test.dart
+++ test/minimize_losses_bot_test.dart
@@ -6,24 +6,40 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:bottino_fortino/api/api.dart';
+import 'package:bottino_fortino/modules/bot/bots/minimize_losses/minimize_losses.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
-import 'package:mockito/annotations.dart';
+// import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
 
-@GenerateMocks([])
+// @GenerateMocks([])
 void main() {
-  final dioAdapter = DioAdapter(dio: Dio());
-  final container = ProviderContainer(
-    overrides: [dioProvider.overrideWithValue(dioAdapter.dio)],
-  );
+  late DioAdapter dioAdapter;
+  late ProviderContainer container;
 
-  setUp(() async {});
+  setUp(() async {
+    dioAdapter = DioAdapter(dio: Dio());
+    container = ProviderContainer(
+      overrides: [dioProvider.overrideWithValue(dioAdapter.dio)],
+    );
+  });
 
   tearDown(() async {
     container.dispose();
   });
 
-  test('Class initialization test', () {});
+  test('Class initialization test', () {
+    // final bot = MinimizeLossesBot.create(
+    //   name: "Bob",
+    //   testNet: true,
+    //   symbol: "BNBUSDT",
+    //   dailyLossSellOrders: 3,
+    //   maxQuantityPerOrder: 100,
+    //   percentageSellOrder: 3,
+    //   timerBuyOrder: const Duration(minutes: 30),
+    // );
+
+    // bot.start(ref);
+  });
 }
