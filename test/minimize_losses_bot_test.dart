ce56c09557ed6e60bc389f592886004c69229b6e
Massimo Tamburini <massimo.tamburini@botika.ai>
2022-04-04 17:32:11 +0200

    refactor(test): added mock api classes

--- test/minimize_losses_bot_test.dart
+++ test/minimize_losses_bot_test.dart
@@ -6,12 +6,46 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:bottino_fortino/api/api.dart';
+import 'package:bottino_fortino/modules/bot/bot.dart';
 import 'package:bottino_fortino/modules/bot/bots/minimize_losses/minimize_losses.dart';
+import 'package:bottino_fortino/modules/settings/settings.dart';
+import 'package:bottino_fortino/providers/providers.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http_mock_adapter/http_mock_adapter.dart';
 // import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
+import 'package:uuid/uuid.dart';
+
+class MockApiProvider implements ApiProvider {
+  @override
+  SpotProvider get spot => MockSpotProvider(MockMarketProvider());
+}
+
+class MockSpotProvider implements SpotProvider {
+  @override
+  MarketProvider market;
+
+  @override
+  late TradeProvider trade;
+
+  @override
+  late WalletProvider wallet;
+
+  MockSpotProvider(this.market);
+}
+
+class MockMarketProvider implements MarketProvider {
+  @override
+  ApiUtils get apiUtils => throw UnimplementedError();
+
+  @override
+  Future<ApiResponse<AveragePrice>> getAveragePrice(
+      ApiConnection conn, String symbol) {
+    return Future.value(const ApiResponse(AveragePrice(0, 20), 200));
+  }
+}
 
 // @GenerateMocks([])
 void main() {
@@ -19,9 +53,14 @@ void main() {
   late ProviderContainer container;
 
   setUp(() async {
+    // For initialize SecureStorage package
+    WidgetsFlutterBinding.ensureInitialized();
     dioAdapter = DioAdapter(dio: Dio());
     container = ProviderContainer(
-      overrides: [dioProvider.overrideWithValue(dioAdapter.dio)],
+      overrides: [
+        dioProvider.overrideWithValue(dioAdapter.dio),
+        apiProvider.overrideWithValue(MockApiProvider())
+      ],
     );
   });
 
@@ -29,17 +68,39 @@ void main() {
     container.dispose();
   });
 
-  test('Class initialization test', () {
-    // final bot = MinimizeLossesBot.create(
-    //   name: "Bob",
-    //   testNet: true,
-    //   symbol: "BNBUSDT",
-    //   dailyLossSellOrders: 3,
-    //   maxQuantityPerOrder: 100,
-    //   percentageSellOrder: 3,
-    //   timerBuyOrder: const Duration(minutes: 30),
-    // );
+  test('Class initialization test', () async {
+    // dioAdapter.onGet('https://testnet.binance.vision/api/v3/avgPrice',
+    //     (server) {
+    //   print('dio bubu');
+    //   server.reply(201, {'dio merda': 'si'});
+    // });
 
-    // bot.start(ref);
+    final foo = container.read(dioProvider);
+
+    // try {
+    //   final res = await foo
+    //       .get<dynamic>('https://testnet.binance.vision/api/v3/avgPrice');
+    // } catch (e) {
+    //   print(e.toString());
+    // }
+
+    final bot = Bot.minimizeLosses(
+      const Uuid().v4(),
+      MinimizeLossesPipeLineData(),
+      name: "BOB",
+      testNet: true,
+      config: MinimizeLossesConfig(
+        symbol: "BNBUSDT",
+        dailyLossSellOrders: 3,
+        maxQuantityPerOrder: 100,
+        percentageSellOrder: 3,
+        timerBuyOrder: const Duration(minutes: 30),
+      ),
+    );
+    container.read(pipelineProvider.notifier).addBots([bot]);
+
+    final pipeline = container.read(pipelineProvider).first;
+
+    pipeline.start();
   });
 }
