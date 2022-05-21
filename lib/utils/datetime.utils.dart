69e36ef8830629f64a1bda6752ca0565a8da1e1f
MassimoTambu <massimo.tamburini.work@gmail.com>
2022-05-21 14:44:58 +0200

    fix(datetime): changed to two digits for seconds

--- lib/utils/datetime.utils.dart
+++ lib/utils/datetime.utils.dart
@@ -2,7 +2,7 @@ import 'package:intl/intl.dart';
 
 class DateTimeUtils {
   static String toHmsddMMy(DateTime dateTime) {
-    final formatter = DateFormat('H:m:s dd-MM-y');
+    final formatter = DateFormat('H:mm:ss dd-MM-y');
     return formatter.format(dateTime.toLocal());
   }
 }
