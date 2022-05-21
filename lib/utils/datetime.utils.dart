4ab2244cf904aaba7a7be7e390ebc0ab73044001
MassimoTambu <massimo.tamburini.work@gmail.com>
2022-05-21 14:43:21 +0200

    fix(datetime): localized datetime

--- lib/utils/datetime.utils.dart
+++ lib/utils/datetime.utils.dart
@@ -3,6 +3,6 @@ import 'package:intl/intl.dart';
 class DateTimeUtils {
   static String toHmsddMMy(DateTime dateTime) {
     final formatter = DateFormat('H:m:s dd-MM-y');
-    return formatter.format(dateTime);
+    return formatter.format(dateTime.toLocal());
   }
 }
